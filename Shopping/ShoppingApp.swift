2fed3a1fcd735da0d3925d24a9fa78b21e801abb
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-04-29 09:37:19 +0500

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Siddikjon Kuziboev on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
